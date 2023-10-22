09e05333744401b65c1fd1997a5c616482f80975
Munjin Nashida <miinjunn@gmail.com>
2023-10-23 03:05:53 +0700

    update content

--- mix_column.py
+++ mix_column.py
@@ -1,14 +1,14 @@
-def gFn(a, b):
+def gFn(state, pre_state):
     p = 0
     hiBitSet = 0
     for i in range(8):
-        if b & 1 == 1:
-            p ^= a
-        hiBitSet = a & 0x80
-        a <<= 1
+        if pre_state & 1 == 1:
+            p ^= state
+        hiBitSet = state & 0x80
+        state <<= 1
         if hiBitSet == 0x80:
-            a ^= 0x1b
-        b >>= 1
+            state ^= 0x1b
+        pre_state >>= 1
     return p % 256
 
 
@@ -24,6 +24,11 @@ def mixColumn(column):
         gFn(temp[1], 1) ^ gFn(temp[0], 3)
 
 
+state_mix_column = [2, 3, 1, 1,
+                    1, 2, 3, 1,
+                    1, 1, 2, 3,
+                    3, 1, 1, 2]
+
 mentahan = ['63', '2f', 'af', 'a2', 'eb', '93', 'c7',
             '20', '9f', '92', 'ab', 'cb', 'a0', 'c0', '30', '2b']
 mentahan_to_int = []
@@ -35,17 +40,73 @@ print()
 print('---------------')
 print()
 
-mixColumn(mentahan_to_int)
-print(mentahan_to_int)
+inp_mix_col = mentahan_to_int.copy()
+mixColumn(inp_mix_col)
+print(inp_mix_col)
 # print(hasil)
 print()
-print("final: ")
 
-hasil = mentahan_to_int.copy()
+hasil = inp_mix_col.copy()
 final = [hex(i)[2:] for i in hasil]
 
 # print(f"hasil: {final[2:]}")
-print(final)
+print(f"hasil (valid cuma row1): {hasil}")
+print(f"final (valid cuma row1): {final}")
+print()
 
 # known issue: hanya mix row pertama saja
 # need to do: add mix column code untuk 3 row sisanya
+
+
+# hasil_mix_column = []
+# for i in range(4):
+#     for j in range(16):
+#         hasil_mix_column[0] = gFn(mentahan_to_int[0], 2) ^ gFn(
+#             mentahan_to_int[1], 3) ^ gFn(mentahan_to_int[2], 1) ^ gFn(mentahan_to_int[3], 1)
+#         hasil_mix_column[1] = gFn(mentahan_to_int[0], 1) ^ gFn(
+#             mentahan_to_int[1], 2) ^ gFn(mentahan_to_int[2], 3) ^ gFn(mentahan_to_int[3], 1)
+#         hasil_mix_column[2]
+#         hasil_mix_column[3]
+#         hasil_mix_column[i] = gFn(mentahan_to_int[i])
+
+
+# temp(x) = mencari tiap row (jadi nnti 4x)
+# akhir = temp1 + temp2 + temp3 + temp4
+#  
+print(f"sebelum di mix: {mentahan_to_int}")
+# hasil_mix_column = [[0 for i in range(16)]]
+hasil_mix_column = []
+print(hasil_mix_column)
+print(len(hasil_mix_column))
+for x in range(4):
+    for i in range(4): #hasilnya  row 1 - kolom 1 sampai 4
+        hasil_mix_column.append( gFn(mentahan_to_int[x*4], state_mix_column[i*4]) ^ gFn(mentahan_to_int[x*4 +1], state_mix_column[i*4 + 1]) ^ gFn(mentahan_to_int[x*4 +2], state_mix_column[i*4 + 2]) ^ gFn(mentahan_to_int[x*4 +3], state_mix_column[i*4 + 3]) )
+    # hasil_mix_column[1] = gFn(mentahan_to_int[0], state_mix_column[i*4]) ^ gFn(mentahan_to_int[1], state_mix_column[i*4 + 1]) ^ gFn(mentahan_to_int[2], state_mix_column[i*4 + 2]) ^ gFn(mentahan_to_int[3], state_mix_column[i*4 + 3])
+    # hasil_mix_column[2] = gFn(mentahan_to_int[0], state_mix_column[i*4]) ^ gFn(mentahan_to_int[1], state_mix_column[i*4 + 1]) ^ gFn(mentahan_to_int[2], state_mix_column[i*4 + 2]) ^ gFn(mentahan_to_int[3], state_mix_column[i*4 + 3])
+    # hasil_mix_column[3] = gFn(mentahan_to_int[0], state_mix_column[i*4]) ^ gFn(mentahan_to_int[1], state_mix_column[i*4 + 1]) ^ gFn(mentahan_to_int[2], state_mix_column[i*4 + 2]) ^ gFn(mentahan_to_int[3], state_mix_column[i*4 + 3])
+
+
+    # hasil_mix_column[4] = gFn(mentahan_to_int[4], state_mix_column[i*4]) ^ gFn(mentahan_to_int[5], state_mix_column[i*4 + 1]) ^ gFn(mentahan_to_int[6], state_mix_column[i*4 + 2]) ^ gFn(mentahan_to_int[7], state_mix_column[i*4 + 3])
+print(f"hasil: {hasil_mix_column}")
+mx_final = [hex(i)[2:] for i in hasil_mix_column]
+print(f"final: {mx_final}")
+
+# index 0 = 0*4
+# index 1 = 0*4 +1
+# index 2 = 0*4 +2
+# index 3 = 0*4 +3
+
+# index 4 = 1*4
+# index 5 = 1*4 +1
+# index 6 = 1*4 +2
+# index 7 = 1*4 +3
+
+# index 8  = 2*4
+# index 9  = 2*4 +1
+# index 10 = 2*4 +2
+# index 11 = 2*4 +3
+
+# index 12 = 3*4
+# index 13 = 3*4 +1
+# index 14 = 3*4 +2
+# index 15 = 3*4 +3
--- shift_row.py
+++ shift_row.py
@@ -1,5 +1,7 @@
-s_matrix = ['63', 'c0', 'ab', '20', 'eb', '2f', '30', 'cb', '9f', '93', 'af', '2b', 'a0', '92', 'c7', 'a2']
+s_matrix = ['63', 'c0', 'ab', '20', 'eb', '2f', '30',
+            'cb', '9f', '93', 'af', '2b', 'a0', '92', 'c7', 'a2']
 # s_matrix = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
 
 # rotate sesuai document(kebawah)
 def rotate(state):
@@ -8,23 +10,43 @@ def rotate(state):
         for j in range(4):
             temp.append(state[j*4 + i])
     return temp
+
+
 rotate_bro = rotate(s_matrix)
 print(rotate_bro)
+
 
 # shift
 def shift(word, n):
     return word[n:]+word[0:n]
 
-def shift_row(state):
+
+def shifting(state):
     shift_r = []
     for i in range(4):
         temp = state[i*4:i*4+4]
         shift_r += shift(temp, i)
     return shift_r
 
-shift_bro = shift_row(rotate_bro)
+
+shift_bro = shifting(rotate_bro)
 print(shift_bro)
 
 # rotate balik
 rotate_balik_bro = rotate(shift_bro)
 print(rotate_balik_bro)
+
+# cara kerja shift-row pada kodingan ini:
+# rotate-shift-rotate
+
+
+def shift_row(state):
+    rot = rotate(state)
+    geser = shifting(rot)
+    shiftRow = rotate(geser)
+    return shiftRow
+
+
+# tes
+r1_shift_row = shift_row(s_matrix)
+print(f"r1 shift-row: {r1_shift_row}")
